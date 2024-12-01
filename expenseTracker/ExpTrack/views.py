fa19096d15c4303efdaffd4aa0d683df256ddbae
Debdeep Das <debdeep22102003@gmail.com>
2024-12-01 20:53:10 +0530

    commit(deb1)
    added my name in views.py

--- expenseTracker/ExpTrack/views.py
+++ expenseTracker/ExpTrack/views.py
@@ -2,6 +2,6 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def check(request):
-    return HttpResponse("Hello, this is the testing page.\nIf you are able to see this, you have successfully loaded the repo.",content_type="text/plain")
+    return HttpResponse("Hello, this is the testing page.\nIf you are able to see this, you have successfully loaded the repo.\nDebdeep",content_type="text/plain")
 
 # Create your views here.
