e1448380d87e22b6837feaa3ab530110628521a4
A1053Bb <abhradeep2019@gmail.com>
2024-12-01 20:42:21 +0530

    initial(1)
    check

--- expenseTracker/ExpTrack/views.py
+++ expenseTracker/ExpTrack/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+
+def check(request):
+    return HttpResponse("Hello, this is the testing page.\nIf you are able to see this, you have successfully loaded the repo.",content_type="text/plain")
 
 # Create your views here.
