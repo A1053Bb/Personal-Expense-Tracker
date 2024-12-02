6e0dad0bc3a501dbb91342da3d71cd2e3ff29e02
siddharth553 <167443645+siddharth553@users.noreply.github.com>
2024-12-02 19:18:50 +0530

    Added my name to HttpResponse in views.py

--- expenseTracker/ExpTrack/views.py
+++ expenseTracker/ExpTrack/views.py
@@ -5,3 +5,7 @@ def check(request):
     return HttpResponse("Hello, this is the testing page.\nIf you are able to see this, you have successfully loaded the repo.\nDebdeep",content_type="text/plain")
 
 # Create your views here.
+
+
+def my_view(request):
+    return HttpResponse("Hello, World!\nSiddharthP")
