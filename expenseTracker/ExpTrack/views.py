c6b2b2372f8f5bb756cad33d66abe8c002a44d4a
A1053Bb <abhradeep2019@gmail.com>
2024-12-02 20:49:26 +0530

    did changes to sidd
    combined them in single check function

--- expenseTracker/ExpTrack/views.py
+++ expenseTracker/ExpTrack/views.py
@@ -2,10 +2,7 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def check(request):
-    return HttpResponse("Hello, this is the testing page.\nIf you are able to see this, you have successfully loaded the repo.\nDebdeep",content_type="text/plain")
+    return HttpResponse("Hello, this is the testing page.\nIf you are able to see this, you have successfully loaded the repo.\nDebdeep\nSiddharthP",content_type="text/plain")
 
 # Create your views here.
 
-
-def my_view(request):
-    return HttpResponse("Hello, World!\nSiddharthP")
