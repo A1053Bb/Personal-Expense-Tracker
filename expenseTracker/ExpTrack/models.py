80ef514a2d70138db40e14259860be18b47c28f9
Debdeep Das <debdeep22102003@gmail.com>
2024-12-03 11:28:52 +0530

    Debdeep
    changes in models.py

--- expenseTracker/ExpTrack/models.py
+++ expenseTracker/ExpTrack/models.py
@@ -1,3 +1,25 @@
 from django.db import models
 
 # Create your models here.
+from django.db import models
+# Create your models here.
+class Employee(models.Model):
+    Empid= models.IntegerField()
+    name= models.CharField(max_length=25)
+    address= models.CharField(max_length=100)
+    salary= models.IntegerField()
+    Department= models.CharField(max_length=25)
+    class Meta:
+        db_table="employee"
+class issue(models.Model):
+      Empid=models.CharField(max_length=50)
+      txn_id=models.CharField(max_length=50)
+      txn_date=models.DateField()
+      charges=models.IntegerField(default=0)
+      class Meta:
+        db_table="issue"
+class login(models.Model):
+      emp_id=models.CharField(max_length=50)
+      pwd=models.CharField(max_length=50)
+      class Meta:
+          db_table="login"
