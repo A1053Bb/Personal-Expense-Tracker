64925b477e691e0985ba8f697c9bf50f3a73b153
A1053Bb <abhradeep2019@gmail.com>
2024-12-04 22:19:14 +0530

    backend login
    implemented backend for login check

--- expenseTracker/ExpTrack/urls.py
+++ expenseTracker/ExpTrack/urls.py
@@ -2,5 +2,9 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("",views.home,name="home"),
+    path("",views.Login,name="login"),
+    path("login",views.Login,name="login"),
+    path("signup",views.signup,name="signup"),
+    path("dashboard",views.dashboard,name="dashboard"),
+    path("logon1",views.logon1)
 ]
--- expenseTracker/ExpTrack/views.py
+++ expenseTracker/ExpTrack/views.py
@@ -1,12 +1,36 @@
 from django.shortcuts import render,redirect
 from django.http import HttpResponse
+from django.contrib import messages
 from ExpTrack.models import *
 
 def check(request):
     return HttpResponse("Hello, this is the testing page.\nIf you are able to see this, you have successfully loaded the repo.\nDebdeep\nSiddharthP\nSiddhartaB",content_type="text/plain")
 
-def home(request):
+def dashboard(request):
     return render(request,"dashboard.html")
+
+def Login(request):
+    return render(request,"index.html")
+
+def signup(request):
+    return render(request,"signup.html")
 
 # Create your views here.
 
+def logon1(request):
+    if request.method == "POST":
+        a = request.POST['email']
+        b = request.POST['password']
+
+        try:
+            user = login.objects.get(emp_id=a)
+            if user.pwd == b :
+                request.session['username'] = user.emp_id
+                return redirect('dashboard')
+            else :
+                messages.error(request,"Invalid password")
+    
+        except login.DoesNotExist :
+            messages.error(request,"User does not exist")
+    
+    return render(request,'index.html')
