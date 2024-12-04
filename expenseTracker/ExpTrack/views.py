6f5de6bbc8e4f697775afb7e5d7de24b6597e016
A1053Bb <abhradeep2019@gmail.com>
2024-12-04 12:05:52 +0530

    dash
    joined dash with django

--- expenseTracker/ExpTrack/views.py
+++ expenseTracker/ExpTrack/views.py
@@ -1,8 +1,12 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
 from django.http import HttpResponse
+from ExpTrack.models import *
 
 def check(request):
     return HttpResponse("Hello, this is the testing page.\nIf you are able to see this, you have successfully loaded the repo.\nDebdeep\nSiddharthP\nSiddhartaB",content_type="text/plain")
+
+def home(request):
+    return render(request,"dashboard.html")
 
 # Create your views here.
 
