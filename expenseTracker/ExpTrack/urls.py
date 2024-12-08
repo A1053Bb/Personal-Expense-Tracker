e7644ecd4452bb9b194ed844e18c0bba405a42af
A1053Bb <abhradeep2019@gmail.com>
2024-12-08 14:25:24 +0530

    signup functionality
    added to signup function and will show user's first name and last name.

--- expenseTracker/ExpTrack/urls.py
+++ expenseTracker/ExpTrack/urls.py
@@ -6,5 +6,7 @@ urlpatterns = [
     path("login",views.Login,name="login"),
     path("signup",views.signup,name="signup"),
     path("dashboard",views.dashboard,name="dashboard"),
-    path("logon1",views.logon1)
+    path("logon1",views.logon1,name="logon1"),
+    path("su",views.su,name="su"),
+    path("logout",views.logout,name='logout')
 ]
--- expenseTracker/ExpTrack/views.py
+++ expenseTracker/ExpTrack/views.py
@@ -7,7 +7,10 @@ def check(request):
     return HttpResponse("Hello, this is the testing page.\nIf you are able to see this, you have successfully loaded the repo.\nDebdeep\nSiddharthP\nSiddhartaB",content_type="text/plain")
 
 def dashboard(request):
-    return render(request,"dashboard.html")
+    emp_id = request.session.get('emp_id')
+    user = login.objects.get(emp_id=emp_id)
+
+    return render(request,"dashboard.html",{'fname':user.firstname,'lname':user.lastname})
 
 def Login(request):
     return render(request,"index.html")
@@ -25,7 +28,7 @@ def logon1(request):
         try:
             user = login.objects.get(emp_id=a)
             if user.pwd == b :
-                request.session['username'] = user.emp_id
+                request.session['emp_id'] = user.emp_id
                 return redirect('dashboard')
             else :
                 messages.error(request,"Invalid password")
@@ -34,3 +37,31 @@ def logon1(request):
             messages.error(request,"User does not exist")
     
     return render(request,'index.html')
+
+def su(request):
+    if request.method == "POST":
+        l = request.POST.get('lname')
+        f = request.POST.get('fname')
+        e = request.POST.get('email')
+        p = request.POST.get('password')
+        c = request.POST.get('confirm-password')
+
+        if p != c :
+            messages.error(request, "Passwords do not match.")
+            return redirect('signup')
+        
+        try :
+            user = login(firstname = f, lastname = l, emp_id = e, pwd = p)
+            user.save()
+            messages.success(request, "Sign-Up Successful!")
+            return redirect('login')
+        
+        except Exception as e :
+            messages.error(request, f"Error: {str(e)}")
+            return redirect("signup")
+    else :
+        return render(request, "signup.html")
+
+def logout(request):
+    request.session.flush()
+    return redirect('login')
